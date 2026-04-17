e574b243d955835127cdc0d91bef080b1eba6218
Megaballs-pip <FATTAKHOV.GLEB@yandex.ru>
2026-04-18 00:36:08 +0300

    Delete web.py

--- web.py
+++
@@ -1,8 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import os
-
-port = int(os.environ.get("PORT", 10000))
-
-with HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler) as httpd:
-    print(f"✅ Веб-сервер запущен на порту {port}")
-    httpd.serve_forever()
